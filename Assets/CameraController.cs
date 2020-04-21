531d966eace098ae1c1fa0dac6716761294aee14
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2020-04-21 20:47:35 +1000

    Initial commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private Transform m_Player = null;
+
+    private void Start()
+    {
+        m_Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {
+        transform.position = m_Player.position;
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    private Rigidbody m_RB;
+
+    private void Start()
+    {
+        m_RB = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        MovePlayer();
+        RotatePlayer();
+    }
+
+    #region Movement System
+    [Header("Movement Controller")]
+    [SerializeField] float m_SpeedMultiplier = 10;
+    [SerializeField] LayerMask m_LayerMask = new LayerMask();
+
+    private float m_InputHorizontal = 0;
+    private float m_InputVertical = 0;
+
+    private void MovePlayer()
+    {
+        m_InputHorizontal = Input.GetAxis("Horizontal");
+        m_InputVertical = Input.GetAxis("Vertical");
+
+        Vector3 _movement = new Vector3(m_InputHorizontal, 0, m_InputVertical) * m_SpeedMultiplier;
+
+        m_RB.velocity = _movement;
+
+    }
+
+    private void RotatePlayer()
+    {
+        RaycastHit _hit;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out _hit, Mathf.Infinity, m_LayerMask))
+            transform.LookAt(new Vector3(_hit.point.x, transform.position.y, _hit.point.z));
+
+    }
+
+
+    #endregion
+}
